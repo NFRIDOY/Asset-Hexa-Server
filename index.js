6345703571e240398ab78a6b33eecf5ab859db9b
Ariful2634 <ariful2634@gmail.com>
2024-01-24 12:58:35 +0600

    create crud operation for transections

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //npm i express cors dotenv
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
 
 // req 
 app.use(express.json());
@@ -14,31 +13,86 @@ app.use(cors({
     credentials: true
 }));
 
-// console.log(process.env.URI)
-const uri = process.env.URI
+
+
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.mq17fxg.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        client.connect();
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-        client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const transectionsCollection = client.db("assethexadb").collection('transections')
+
+    // create
+
+    app.post('/transections',async(req,res)=>{
+        const newTransections = req.body;
+        // console.log(newTransections)
+        const result = await transectionsCollection.insertOne(newTransections);
+        res.send(result)
+    })
+
+     // read
+
+     app.get('/transections',async(req,res)=>{
+        const cursor = transectionsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+     // delete
+
+     app.delete('/transections/:id',async(req,res)=>{
+        const id  = req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await transectionsCollection.deleteOne(query);
+        res.send(result)
+    })
+
+    // find
+
+    app.get('/transections/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await transectionsCollection.findOne(query);
+        res.send(result)
+    })
+
+    // update
+
+    app.put('/transections/:id', async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const options = { upsert: true };
+        const updateTransections = req.body
+        const transections={
+            $set:{
+                 // TODO: update property
+              
+            }
+        }
+        const result = await transectionsCollection.updateOne(filter,transections,options);
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
