e24f427677fc55330259c5b9e20c41b27bc12721
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-01-25 16:24:06 +0600

    update and comment client.connect

--- index.js
+++ index.js
@@ -31,8 +31,12 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
-        const usersCollection = client.db('assethexadb').collection('users')
         const database = client.db("assethexadb");
+
+
+        
+
+        const usersCollection = client.db('assethexadb').collection('users')
 
         const transectionsCollection = database.collection('transections')
         const accountsCollection = database.collection('accounts')
@@ -40,32 +44,32 @@ async function run() {
 
 
 
-        
-    // Save or modify user email, status in DB
-    app.put('/users/:email', async (req, res) => {
-        const email = req.params.email
-        const user = req.body
-        const query = { email: email }
-        const options = { upsert: true }
-        const isExist = await usersCollection.findOne(query)
-        console.log('User found?----->', isExist)
-        if (isExist) return res.send(isExist)
-        const result = await usersCollection.updateOne(
-          query,
-          {
-            $set: { ...user, timestamp: Date.now() },
-          },
-          options
-        )
-        res.send(result)
-      })
+
+        // Save or modify user email, status in DB
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const query = { email: email }
+            const options = { upsert: true }
+            const isExist = await usersCollection.findOne(query)
+            console.log('User found?----->', isExist)
+            if (isExist) return res.send(isExist)
+            const result = await usersCollection.updateOne(
+                query,
+                {
+                    $set: { ...user, timestamp: Date.now() },
+                },
+                options
+            )
+            res.send(result)
+        })
 
 
-       // Get all users
-    app.get('/users',  async (req, res) => {
-        const result = await usersCollection.find().toArray()
-        res.send(result)
-      })
+        // Get all users
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result)
+        })
 
 
 
@@ -74,7 +78,7 @@ async function run() {
         // client.connect();
         // Send a ping to confirm a successful connection
 
-     
+
         // for transection
         // create
 
