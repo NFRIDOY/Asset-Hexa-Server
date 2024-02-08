c7027faef62850ad6edaac20f3079720a6659836
Mustafiz82e <mustafiz8260@gamil.com>
2024-02-08 22:16:02 +0600

    admin api for blog and user varification created

--- index.js
+++ index.js
@@ -938,6 +938,59 @@ async function run() {
       }
     });
 
+
+    //--------------------------- Admin Dashboard Api -------------------------
+
+
+    app.put('/user/:email' ,async(req , res) =>{
+
+        const email = req.params.email
+        // const updateUser = {isVerified : "true"}
+        // console.log(email ,updateUser)
+
+        const filter = { email : email};
+        const options ={ upsert: true };
+        const updateDoc = {
+          $set: {
+            isVerified : "true"
+          },
+        };
+
+        const result = await usersCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+        console.log(result)
+  
+      })
+
+      
+    app.put('/blog/:id' ,async(req , res) =>{
+
+        const id = req?.params.id
+        console.log(id);
+
+        const filter = {_id : new ObjectId(id)};
+        const options ={ upsert: true };
+        const updateDoc = {
+          $set: {
+            isVerified : "true"
+          },
+        };
+
+        const result = await blogCollection.updateOne(filter, updateDoc, options);
+        res.send(result)
+        console.log(result)
+  
+      })
+  
+
+
+
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
